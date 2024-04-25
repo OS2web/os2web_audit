b061683134af0d755c036dc8ec6b69988f1a7c45
Jesper Kristensen <cableman@linuxdev.dk>
2024-04-25 13:25:44 +0200

    ITKDev: Added better logging options

--- src/Service/Logger.php
+++ src/Service/Logger.php
@@ -21,6 +21,48 @@ class Logger {
   }
 
   /**
+   * Logs a message at info level.
+   *
+   * @param string $type
+   *   The type of event to log (auth, lookup etc.)
+   * @param int $timestamp
+   *   The timestamp for the log message.
+   * @param string $line
+   *   The log message.
+   * @param bool $logUser
+   *   Log information about the current logged-in user (need to track who has
+   *   lookup information in external services). Default: false.
+   * @param array $metadata
+   *   Additional metadata for the log message. Default is an empty array.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\PluginException
+   */
+  public function info(string $type, int $timestamp, string $line, bool $logUser = false, array $metadata = []): void {
+    $this->log($type, $timestamp, $line, $logUser, $metadata + ['level' => 'info']);
+  }
+
+  /**
+   * Logs a message at error level.
+   *
+   * @param string $type
+   *   The type of event to log (auth, lookup etc.)
+   * @param int $timestamp
+   *   The timestamp for the log message.
+   * @param string $line
+   *   The log message.
+   * @param bool $logUser
+   *   Log information about the current logged-in user (need to track who has
+   *   lookup information in external services). Default: false.
+   * @param array $metadata
+   *   Additional metadata for the log message. Default is an empty array.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\PluginException
+   */
+  public function error(string $type, int $timestamp, string $line, bool $logUser = false, array $metadata = []): void {
+    $this->log($type, $timestamp, $line, $logUser, $metadata + ['level' => 'error']);
+  }
+
+  /**
    * Logs a message using a plugin-specific logger.
    *
    * @param string $type
@@ -29,12 +71,15 @@ class Logger {
    *   The timestamp for the log message.
    * @param string $line
    *   The log message.
+   * @param bool $logUser
+   *   Log information about the current logged-in user (need to track who has
+   *   lookup information in external services). Default: false.
    * @param array $metadata
    *   Additional metadata for the log message. Default is an empty array.
    *
    * @throws \Drupal\Component\Plugin\Exception\PluginException
    */
-  public function log(string $type, int $timestamp, string $line, array $metadata = []): void {
+  private function log(string $type, int $timestamp, string $line, bool $logUser = false, array $metadata = []): void {
     $config = $this->configFactory->get(SettingsForm::$configName);
     $plugin_id = $config->get('provider');
 
@@ -42,6 +87,12 @@ class Logger {
     // @todo: Fallback logger on error.
     $configuration = $this->configFactory->get(PluginSettingsForm::getConfigName())->get($plugin_id);
     $logger = $this->loggerManager->createInstance($plugin_id, $configuration ?? []);
+
+    if ($logUser) {
+      // Add user id to the log message metadata.
+      $user = \Drupal::currentUser();
+      $metadata['userId'] = $user->id();
+    }
 
     $logger->log($type, $timestamp, $line, $metadata);
   }
