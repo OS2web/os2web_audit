0bba48b65ba5f32de5483fe811107183efc51a5a
Sine Jespersen <sinejespersen@gmail.com>
2025-02-07 12:48:39 +0100

    add 12 to drush version, clean up command

--- src/Drush/Commands/Commands.php
+++ src/Drush/Commands/Commands.php
@@ -2,13 +2,13 @@
 
 namespace Drupal\os2web_audit\Drush\Commands;
 
+use Drush\Attributes\Command;
 use Drupal\os2web_audit\Service\Logger;
 use Drush\Commands\DrushCommands;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
-use Drush\Attributes\Option;
-use Drush\Attributes\Command;
+use Drush\Attributes\Argument;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-
+use Drush\Exceptions\CommandFailedException;
 /**
  * Simple command to send log message into audit log.
  */
@@ -24,9 +24,10 @@ class Commands extends DrushCommands {
     #[Autowire(service: 'os2web_audit.logger')]
     protected readonly Logger $auditLogger,
   ) {
+    parent::__construct();
   }
 
-    /**
+  /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container): self {
@@ -35,26 +36,19 @@ class Commands extends DrushCommands {
     );
   }
 
-
   /**
    * Log a test message to the os2web_audit logger.
    *
-   * @param string $log_message
-   *   Message to be logged.
-   *
-   * @command audit:log
-   * @usage audit:log 'This is a test message'
-   *   Logs 'This is a test message' to the os2web_audit logger.
-   *
    * @throws \Drupal\Component\Plugin\Exception\PluginException
-   * @throws \Exception
+   * @throws CommandFailedException
    */
-   #[Command(name: 'audit:log')]
-   #[Option(name: 'log_message', description: "The test message to be logged")]
-  public function logMessage(string $log_message = ''): void {
+  #[Command(name: 'audit:log')]
+  #[Argument(name: 'log_message', description: "Message to be logged.")]
+  public function logMessage(string $log_message): void {
     if (empty($log_message)) {
-      throw new \Exception('Log message cannot be empty.');
+      throw new CommandFailedException('Log message cannot be empty.');
     }
+
     $this->auditLogger->info('test', $log_message, FALSE, ['from' => 'drush']);
     $this->auditLogger->error('test', $log_message, TRUE, ['from' => 'drush']);
   }
